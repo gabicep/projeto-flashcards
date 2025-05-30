adc8502544a77a043e6501811c74e6a90a40f4a9
gabicep <gabrielle.ronchi.batista@escola.pr.gov.br>
2025-05-30 20:43:25 -0300

    Começando aula 09

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao (
+    'Português',
+    'Qual é o termo da oração que substitui o nome de uma pessoa ou coisa?',
+    'O termo que substitui é o pronome'
+)
+
+criaCartao (
+    'Português',
+    'Quais os termos essenciais de uma oração?',
+    'Os termos essenciais são o sujeito e o predicado'
+)
+
+criaCartao (
+    'Geografia',
+    'Qual a capital da Espanha?',
+    'A capital da Espanha é Madrid'
+)
+
+criaCartao (
+    'Geografia',
+    'Qual o pico mais alto do mundo?',
+    'O pico mais alto do mundo é o Monte Everest'
+)
