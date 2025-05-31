0907fc68aa90ea81e32693910632f9627a2738d2
gabicep <gabrielle.ronchi.batista@escola.pr.gov.br>
2025-05-30 22:41:32 -0300

    Projeto finalizado

--- perguntas.js
+++ perguntas.js
@@ -1,11 +1,5 @@
 criaCartao (
     'Português',
-    'Qual é o termo da oração que substitui o nome de uma pessoa ou coisa?',
-    'O termo que substitui é o pronome'
-)
-
-criaCartao (
-    'Português',
     'Quais os termos essenciais de uma oração?',
     'Os termos essenciais são o sujeito e o predicado'
 )
@@ -18,6 +12,36 @@ criaCartao (
 
 criaCartao (
     'Geografia',
-    'Qual o pico mais alto do mundo?',
-    'O pico mais alto do mundo é o Monte Everest'
+    'Qual o menor país do mundo?',
+    'O menor país do mundo é o Vaticano'
+)
+
+criaCartao (
+    'Língua Inglesa',
+    'Como se diz "bom dia" em inglês?',
+    'Em inglês se diz "good morning"'
+)
+
+criaCartao (
+    'Língua Inglesa',
+    'Qual a tradução de "The book is on the table"?',
+    'Em português é "O livro está sobre a mesa"'
+)
+
+criaCartao (
+    'História',
+    'Quando Dom Pedro I proclamou a independência do Brasil?',
+    'Dom Pedro I proclamou a independência do Brasil em 7 de setembro de 1822'
+)
+
+criaCartao (
+    'História',
+    'Qual o nome do presidente do Brasil que ficou conhecido como Jango?',
+    'O presidente que ficou conhecido como Jango foi o João Goulart'
+)
+
+criaCartao (
+    'Bônus',
+    'Qual o livro mais vendido no mundo depois da Bíblia?',
+    'O segundo livro mais vendido do mundo é o Dom Quixote'
 )
